9ab10ea3d5e660de44ede73bb80bc029a7816ac0
ajay8106 <52490796+ajay8106@users.noreply.github.com>
2020-05-30 11:15:12 +0530

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+#print(pyautogui.position())
+#to get the position of curcer.
+
+i=0
+while i<50:
+    pyautogui.click(1627,975)
+    time.sleep(0.2)
+    i+=1
+    
